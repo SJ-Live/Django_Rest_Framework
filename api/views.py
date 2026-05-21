67cb967fb8570b5ae6848bd37824f5883f71afb4
SJ-Live <shahjamal570@gmail.com>
2026-05-21 21:50:10 +0530

    viewsets => Viewset

--- api/views.py
+++ api/views.py
@@ -9,7 +9,8 @@ from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from Employees.models import Employee
 from django.http import Http404
-from rest_framework import mixins, generics
+from django.shortcuts import get_object_or_404
+from rest_framework import mixins, generics, viewsets
 # Create your views here.
 
 @api_view(['GET','POST']) # This decorator is used to specify that the studentsView function should only handle defined requests. If a different type of request is made to this view, it will return a 405 Method Not Allowed response.
@@ -118,6 +119,8 @@ class EmployeeDetail(mixins.RetrieveModelMixin,mixins.UpdateModelMixin,mixins.De
     def delete(self, request, pk):
         return self.destroy(request, pk)
 """
+
+"""
 class Employees(generics.ListCreateAPIView):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
@@ -127,3 +130,36 @@ class EmployeeDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
     lookup_field = 'pk' # This tells the view to use the 'pk' field to look up the Employee object. By default, generics.RetrieveAPIView uses 'pk' as the lookup field, so this line is actually optional in this case. However, if you wanted to use a different field (e.g., 'emp_id'), you would set lookup_field = 'emp_id'.
+
+"""
+
+class EmployeeViewset(viewsets.ViewSet):
+    def list(self, request):
+        qweryset = Employee.objects.all()
+        serializer = EmployeeSerializer(qweryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def create(self, request):
+        serializer = EmployeeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def retrieve(self, request, pk=None):
+        employee = get_object_or_404(Employee, pk=pk)
+        serializer = EmployeeSerializer(employee)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def update(self, request, pk=None):
+        employee = get_object_or_404(Employee, pk=pk)
+        serializer  = EmployeeSerializer(employee, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def destroy(self, request, pk=None):
+        employee = get_object_or_404(Employee, pk=pk)
+        employee.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
